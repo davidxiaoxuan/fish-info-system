8c4cc35e7aa6fc0f0c4b88ed936ba6b3a2b52394
Xiaoxuan Chen <x.chen643@student.fdu.edu>
2024-07-18 14:52:48 -0700

    example name update

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -5,7 +5,8 @@ const HomePage = () => {
     return (
         <div className="homepage">
             <h1>Welcome to the Fish Information System</h1>
-            <p>Select a region to see the fish information.</p>
+            <p>Select a region to see the fish information, or search by entering the name of the fish below.</p>
+            <p>Ambloplites rupestris, Luxilus cornutus, Nocomis micropogon, Umbra limi, Rhinichthys atratulus</p>
         </div>
     );
 };
